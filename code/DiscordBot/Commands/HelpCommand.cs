093c7cd22209e056f2aa4e01694aec9d024626d3
TortoiseEatToes <saralynnasmussen@gmail.com>
2025-01-08 18:29:04 -0800

    Inverting the return on the GetHelp command

--- code/DiscordBot/Commands/HelpCommand.cs
+++ code/DiscordBot/Commands/HelpCommand.cs
@@ -18,19 +18,18 @@ namespace Tortoise
 
         public bool HandleTextCommand(TortoiseBot tortoiseBot, SocketCommandContext commandContext, string[] splitContent)
         {
-            bool bHandled = false;
-            if (splitContent[0].ToLower() == "help")
+            if (splitContent[0].ToLower() != "help")
             {
-                string outputMessage = "";
-                outputMessage += "Hello!  I'm TortoiseBot!  I'm here to help with game jams!\n";
-                outputMessage += "- Use '!unity' to use the Unity command with the args: !unity projectName unityVersion unityMethod additionalArguments\n";
-                string rolesChannel = $"<#{tortoiseBot.GetSettings().reactChannel}>";
-                outputMessage += $"- You can also react to react to the roles message in {rolesChannel} to get roles.\n";
-
-                commandContext.Channel.SendMessageAsync(outputMessage);
-                bHandled = true;
+                return false;
             }
-            return bHandled;
+            string outputMessage = "";
+            outputMessage += "Hello!  I'm TortoiseBot!  I'm here to help with game jams!\n";
+            outputMessage += "- Use '!unity' to use the Unity command with the args: !unity projectName unityVersion unityMethod additionalArguments\n";
+            string rolesChannel = $"<#{tortoiseBot.GetSettings().reactChannel}>";
+            outputMessage += $"- You can also react to react to the roles message in {rolesChannel} to get roles.\n";
+
+            commandContext.Channel.SendMessageAsync(outputMessage);
+            return true;
         }
     }
 }
